045092c46a31883a5939cf5d1cadb497b7758654
lorenzodisanto <lore.disanto95@gmail.com>
2024-01-31 15:29:16 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      title: "Todolist",
+
+      tasks: [
+        {
+          text: "Latte",
+          done: false,
+        },
+        {
+          text: "Caffè",
+          done: true,
+        },
+        {
+          text: "Biscotti",
+          done: true,
+        },
+        {
+          text: "Cioccolato",
+          done: false,
+        },
+        {
+          text: "Carne",
+          done: false,
+        },
+        {
+          text: "Gelato",
+          done: true,
+        },
+        {
+          text: "Pasta",
+          done: false,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    printHello() {
+      console.log("hello vue");
+    },
+  },
+
+  mounted() {
+    this.printHello();
+  },
+}).mount("#app");
